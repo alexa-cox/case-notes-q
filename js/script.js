a282bc26bcf4cb1cafd2e9af81566c60403ad08f
Alexandria Cox <alexacox.atc@gmail.com>
2024-11-19 01:31:09 -0500

    feat: :sparkles: Create and copy prompt to clipboard

--- js/script.js
+++ js/script.js
@@ -9,3 +9,58 @@ function passwordCopy(e) {
   let passValue = password.value;
   navigator.clipboard.writeText(passValue);
 }
+
+// PROMPT BUILD
+const summary = document.querySelector('#summary');
+const action = document.querySelector('#action');
+const resource = document.querySelector('#resource');
+const resolve = document.querySelector('#resolve');
+const promptCopyBtn = document.querySelector('#promptCopy');
+promptCopyBtn.addEventListener('click', promptCopy);
+
+let summaryValue = '';
+let actionValue = '';
+let resourceValue = '';
+let resolveValue = '';
+
+// Copy license text to clipboard
+function promptCopy(e) {
+  e.preventDefault();
+
+  summaryValue = summary.value;
+  actionValue = action.value;
+  resourceValue = resource.value;
+  resolveValue = resolve.options[resolve.selectedIndex].text;
+  clipBoard(summaryValue, actionValue, resourceValue, resolveValue);
+}
+
+function clipBoard(sum, act, resourse, resolve) {
+  // PROMPT
+  const prompt = `Using this format:
+
+S-Situation: What's the end goal of the customer? The situation should be what they want to ultimately achieve. I.e. balance check register
+
+T-Trouble: What prevents the customer from completing the task?
+
+A-Actions Taken: What resources and actions solved the concern?
+Resources used: Include all resources used. 
+
+R-Resolution: What solution was provided? This is the customer’s status at the close of the interaction i.e. was a full resolution reached during the contact? Where should the next agent pick back up should the customer contact reach out again? If a full resolution was reached, then a single line stating Issue resolved with a bit of context is all that is needed.
+
+Remember the situation should be what the customer wants to do. In other words what can they do after the issue is resolved. 
+
+Take these notes and summary and write my case notes:
+-------------------------------------------------------
+${sum}
+
+Actions we took:
+${act}
+
+Resources used:
+${resourse}
+
+Was it resolved: ${resolve}
+`;
+
+  navigator.clipboard.writeText(prompt);
+}
